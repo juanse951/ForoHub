f8627f621f1c2dbb75a8c9c374ddb3db5679fff6
juanse951 <juanse951@gmail.com>
2025-01-02 19:58:56 -0500

    se verifica en setting/compile/build project automatically y Advanced Setting/compiler/Allow Auto-make... para que pueda cargar correctamente en el localHost

--- src/main/java/com/aluracursos/Foro/Hub/ForoHubApplication.java
+++ src/main/java/com/aluracursos/Foro/Hub/ForoHubApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ForoHubApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ForoHubApplication.class, args);
 	}
