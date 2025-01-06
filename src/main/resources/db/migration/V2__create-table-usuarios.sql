3fd5d91fe5ea22cb3ec1806bb180f091c531e6a7
juanse951 <juanse951@gmail.com>
2025-01-05 22:27:07 -0500

    se modifican tablas de migraciones y el pom ya que no funcionaban lombok y las dependencias de security no me dejaban iniciar

--- src/main/resources/db/migration/V2__create-table-usuarios.sql
+++ src/main/resources/db/migration/V2__create-table-usuarios.sql
@@ -1,7 +1,7 @@
 CREATE TABLE usuarios (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
-    correoElectronico VARCHAR(255) NOT NULL,
+    correo_electronico VARCHAR(255) NOT NULL,
     contrasena VARCHAR(255) NOT NULL,
     perfil_id BIGINT,
     FOREIGN KEY (perfil_id) REFERENCES perfiles(id)
