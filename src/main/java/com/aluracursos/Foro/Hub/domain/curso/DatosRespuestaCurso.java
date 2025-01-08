b9b161f98e3f0d72cd1762863c94b37e5c28adb4
juanse951 <juanse951@gmail.com>
2025-01-07 21:39:24 -0500

    se agregan anotaciones a las respuestas jeje

--- src/main/java/com/aluracursos/Foro/Hub/domain/curso/DatosRespuestaCurso.java
+++ src/main/java/com/aluracursos/Foro/Hub/domain/curso/DatosRespuestaCurso.java
@@ -1,7 +1,10 @@
 package com.aluracursos.Foro.Hub.domain.curso;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record DatosRespuestaCurso(
 
+        @NotBlank(message = "{nombre.obligatorio}")
         String nombre
 ) {
 }
--- src/main/java/com/aluracursos/Foro/Hub/domain/usuario/DatosRespuestaUsuario.java
+++ src/main/java/com/aluracursos/Foro/Hub/domain/usuario/DatosRespuestaUsuario.java
@@ -1,7 +1,10 @@
 package com.aluracursos.Foro.Hub.domain.usuario;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record DatosRespuestaUsuario(
 
+        @NotBlank(message = "{nombre.obligatorio}")
         String nombre
 ) {
 }
