a341fec76d2215f62132d7a31f3476e10ef07d29
juanse951 <juanse951@gmail.com>
2025-01-11 14:20:30 -0500

    se implementan los metodos para retornar usuarios mysql pero aun me queda resolver por que no puedo ingresar manualmente una clasve a mi bd y se sobreescribe el defaultpassword

--- src/main/java/com/aluracursos/Foro/Hub/controller/AutenticacionController.java
+++ src/main/java/com/aluracursos/Foro/Hub/controller/AutenticacionController.java
@@ -1,12 +1,14 @@
 package com.aluracursos.Foro.Hub.controller;
 
 import com.aluracursos.Foro.Hub.domain.usuario.DatosAutenticacionUsuario;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class AutenticacionController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping
-    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario){
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
         Authentication token = new UsernamePasswordAuthenticationToken(
                 datosAutenticacionUsuario.correoElectronico(),datosAutenticacionUsuario.contrasena());
         authenticationManager.authenticate(token);
--- src/main/java/com/aluracursos/Foro/Hub/domain/usuario/Usuario.java
+++ src/main/java/com/aluracursos/Foro/Hub/domain/usuario/Usuario.java
@@ -5,8 +5,12 @@ import com.aluracursos.Foro.Hub.domain.topico.Topico;
 import com.aluracursos.Foro.Hub.domain.usuario.perfil.Perfil;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Table(name = "usuarios")
@@ -16,7 +20,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of="id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,4 +49,38 @@ public class Usuario {
         this.nombre = datos.nombre();
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return correoElectronico;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
