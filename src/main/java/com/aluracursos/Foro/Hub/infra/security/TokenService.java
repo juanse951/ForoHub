9a475fd516623106da8c14d850befd278d573719
juanse951 <juanse951@gmail.com>
2025-01-25 11:00:31 -0500

    se pone un valor predeterminado para el JWT

--- src/main/java/com/aluracursos/Foro/Hub/infra/security/TokenService.java
+++ src/main/java/com/aluracursos/Foro/Hub/infra/security/TokenService.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
