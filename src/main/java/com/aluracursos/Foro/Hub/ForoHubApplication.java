7dd62f587ebc194f417d60ac76f59f9b650022bb
juanse951 <juanse951@gmail.com>
2025-01-26 01:59:32 -0500

    prueba 2 se modifica el properties, hay una configuración de CORS (Cross-Origin Resource Sharing) para permitir solicitudes desde otros orígenes. Se permite el acceso para los métodos HTTP especificados (HEAD, GET, PUT, POST, DELETE, PATCH).

--- src/main/java/com/aluracursos/Foro/Hub/ForoHubApplication.java
+++ src/main/java/com/aluracursos/Foro/Hub/ForoHubApplication.java
@@ -2,6 +2,10 @@ package com.aluracursos.Foro.Hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ForoHubApplication {
@@ -11,4 +15,17 @@ public class ForoHubApplication {
 		SpringApplication.run(ForoHubApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
